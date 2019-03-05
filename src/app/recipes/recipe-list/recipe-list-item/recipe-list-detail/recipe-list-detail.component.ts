1c264e7c274127b064cbae7d53c51a04abbf09db
Kelly Diabagate <diabagatekelly@yahoo.fr>
2019-03-05 00:12:29 -0600

    Finished recipe detail section with recipe name, image, and dropdown for adding, editing and deleting recipes

--- src/app/recipes/recipe-list/recipe-list-item/recipe-list-detail/recipe-list-detail.component.ts
+++ src/app/recipes/recipe-list/recipe-list-item/recipe-list-detail/recipe-list-detail.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../../../recipe.model';
+
 @Component({
   selector: 'app-recipe-list-detail',
   templateUrl: './recipe-list-detail.component.html',
   styleUrls: ['./recipe-list-detail.component.css']
 })
 export class RecipeListDetailComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A new recipe', 'This is a test recipe',
+     'https://images.pexels.com/photos/716244/pexels-photo-716244.jpeg?cs=srgb&dl=aloo-aloo-poha-breakfast-716244.jpg&fm=jpg'),
+     new Recipe('A second recipe', 'Second test recipe', 'http://www.picserver.org/pictures/salad03-lg.jpg')
+  ];
 
   constructor() { }
 
